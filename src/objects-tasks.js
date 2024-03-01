68c82efa94c1a7e4dd1615ea89b34da1f0bce744
Grigori Konopelko <grigori.konopelko@gmail.com>
2024-03-01 10:00:05 +0300

    2

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,10 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  let result = {};
+  result = Object.assign(result, obj);
+  return result;
 }
 
 /**
@@ -32,8 +34,18 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+  objects.forEach((item) => {
+    Object.entries(item).forEach(([key]) => {
+      if (key in result) {
+        result[key] += item[key];
+      } else {
+        result[key] = item[key];
+      }
+    });
+  });
+  return result;
 }
 
 /**
